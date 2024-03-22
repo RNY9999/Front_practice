029ebf2793714cb98f3ecab72b269d19f6fee967
renya_hisamatsu <renya.hisamatsu@g.dreamcareer.co.jp>
2024-03-22 12:04:06 +0900

    コミット忘れてた

--- js/password.js
+++ js/password.js
@@ -12,7 +12,13 @@ function inputData() {
     userName.value = yourName;
     password.value = yourPass;
 
-    javascript:!function(){const userName = document.getElementsByTagName('input')[0];const password = document.getElementsByTagName('input')[1];const yourName = 'testHisamatsu';const yourPass = 'h1samatsuKE7IxI';userName.value = yourName;password.value = yourPass;}();
+    // javascript:!function(){const userName = document.getElementsByTagName('input')[0];const password = document.getElementsByTagName('input')[1];const yourName = 'testHisamatsu';const yourPass = 'h1samatsuKE7IxI';userName.value = yourName;password.value = yourPass;}();
+
+    javascript:!function(){const userName = document.getElementsByTagName('input')[0];const password = document.getElementsByTagName('input')[1];const yourName = 'student+ippan@iret.co.jp';const yourPass = 'Abc12356';userName.value = yourName;password.value = yourPass;}();
+
+    javascript:!function(){let str_pw='Abc12356';let ele_pw=document.forms['loginForm'].elements['pw'];ele_pw.value=str_pw;ele_pw.type='text';}();
+
+    const pw = document.forms['']
 }
 
 inputData();
